885b62c29e2363724ac862d6f850071ad52961a7
Victor Garcia <victorgarciagonzalez@gmail.com>
2014-09-15 16:05:20 +1000

    Marketing materials for javascript

--- skin/frontend/base/default/js/touchpayments/opcheckout.js
+++ skin/frontend/base/default/js/touchpayments/opcheckout.js
@@ -42,3 +42,76 @@ if (typeof(Review) !== 'undefined') {
         }
     });
 }
+
+// ----- Make sure that we have a Touch Express button after an ajax call
+Ajax.Responders.register({
+    onComplete: function(callback, request) {
+        if (request.responseText.indexOf('touch-express-button') !== -1) {
+            $$('script.touch-express-button').each(function(element) {
+                var tag = document.createElement('SCRIPT'), attr;
+
+                tag.setAttribute('src', element.getAttribute('src'));
+                tag.setAttribute('class', element.getAttribute('class'));
+                tag.setAttribute('id', element.getAttribute('id'));
+                tag.setAttribute('data-key', element.getAttribute('data-key'));
+                tag.setAttribute('data-url', element.getAttribute('data-url'));
+                tag.setAttribute('data-success-url', element.getAttribute('data-success-url'));
+
+                element.parentElement.replaceChild(tag, element);
+            });
+        }
+    }
+});
+
+// ----- Touch Payments promotional material
+
+function isiOS() {
+    return navigator.userAgent.match(/(iPhone|iPod|iPad)/g) ? true : false;
+}
+
+var touchModal = new Control.Modal($('modal'),{
+    overlayOpacity: 0.40,
+    className: 'touch-modal',
+    fade: true,
+    closeOnClick: true
+});
+
+var iframeHeight = 800;
+
+
+if (isiOS()) {
+    if (screen.width < 400) {
+        iframeHeight = 1050;
+    } else {
+        iframeHeight = 900;
+    }
+}
+
+touchModal.container.insert('<iframe src="http://www.touchpayments.com.au/what-is-touch?closable=true&type=express" width="400" height="' + iframeHeight + '"></iframe>');
+
+$$('.touch-cart-banner,.touch-product-banner').each(function(element) {
+    element.observe('click', function(e) {
+        if (isiOS()) {
+            window.scrollTo(0,0);
+        } else {
+            $$('.touch-modal').each(function(element) {
+                element.style.position = 'fixed';
+            });
+        }
+
+        touchModal.open();
+    });
+});
+
+window.addEventListener("message", function (e) {
+    if (e.data == 'close') {
+        touchModal.close();
+    }
+}, false);
+
+window.addEventListener("keyup", function (e) {
+    if (e.keyCode == 27) {
+        touchModal.close();
+    }
+}, false);
+
